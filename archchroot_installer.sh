1fed1fe9db3b02f3445e7e753164ae07567b6daa
Thiago Portella <52546576+jamespll@users.noreply.github.com>
2021-03-26 02:30:22 -0300

    Update archchroot_installer.sh

--- archchroot_installer.sh
+++ archchroot_installer.sh
@@ -11,7 +11,7 @@ echo -e "\n127.0.0.1\tlocalhost\n::1\t\tlocalhost\n127.0.1.1\tjamespll-arch.loca
 
 passwd
 
-pacman -S grub grub-btrfs efibootmgr networkmanager network-manager-applet wireless_tools wpa_supplicant dialog os-prober mtools dosfstools base-devel linux-headers reflector xorg gdm xf86-video-qxl mesa gnome firefox xdg-utils
+pacman -S grub grub-btrfs efibootmgr networkmanager network-manager-applet wireless_tools wpa_supplicant dialog os-prober mtools dosfstools base-devel linux-headers reflector xorg gdm xf86-video-qxl mesa gnome xdg-utils firefox bat vifm wget
 
 #intel: xf86-video-intel libgl
 #amd: xf86-video-amdgpu
