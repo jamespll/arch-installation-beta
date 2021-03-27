320e76938a26761f5d19002c622444e52ab8ac86
Thiago Portella <52546576+jamespll@users.noreply.github.com>
2021-03-27 01:17:00 -0300

    Update and rename arch_installation_btrfs.txt to arch_installation_btrfs.sh

--- arch_installation_btrfs.sh
+++ arch_installation_btrfs.sh
@@ -2,27 +2,30 @@
  
 # create snapper config
 umount /.snapshots/
-rm -rf /.snapshots/
+rm -r /.snapshots/
 snapper -c root create-config /
+sudo btrfs subvolume delete /.snapshots
+mkdir /.snapshots
+mount -a
+chmod 750 /.snapshots
 
-# allow permissions on snapper: add 'jamespll' in ALLOW_USERS=""
+
+# allow permissions on snapper: add 'jamespll' in ALLOW_USERS="" and edit limits for timeline cleanup (hourly 5, daily 7)
 # e.g: ALLOW_USERS="jamespll"
 vim /etc/snapper/configs/root
 
 # configure snapshots timeline and grub
-chmod a+rx /.snapshots/
-systemctl start snapper-timeline.timer
-systemctl enable snapper-timeline.timer
-systemctl enable snapper-cleanup.timer
-systemctl start snapper-cleanup.timer
-systemctl start grub-btrfs.path
-systemctl enable grub-btrfs.path
+sudo chmod a+rx /.snapshots/
+sudo chown :jamespll /.snapshots
+systemctl enable --now snapper-timeline.timer
+systemctl enable --now snapper-cleanup.timer
+systemctl enable --now grub-btrfs.path
 
 # reboot
 # sudo reboot -h now
 
 # create the first snapshot on the system
-snapper -c root create -c timeline --description "After Install"
+snapper -c root create -c timeline --description "After install"
 
 # update and install pacman hook
 sudo pacman -Syu
